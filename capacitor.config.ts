2b4eeec50dda748c4e6768a5f15cbb6097d24744
MarianaC9910 <21152@portalcedi.edu.mx>
2025-03-18 08:43:14 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'esa20parcial2',
+  webDir: 'www'
+};
+
+export default config;
